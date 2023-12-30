f89e21c9492cbd54052f708f7b9b6ae189ee886d
devbymarcos <marcoslopes.dev@gmail.com>
2023-12-30 17:53:00 -0300

    feat: props name

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,14 +1,18 @@
 interface Props {
   type: string;
   value?: string;
+  placeholder?: string;
+  name: string;
 }
 
-const Input = ({ type, value }: Props) => {
+const Input = ({ type, value, placeholder, name }: Props) => {
   return (
     <input
       type={type}
       className="rounded-md w-full py-3 px-2 outline-none  bg-base-white dark:text-base-white text-base-black dark:bg-base-black border border-base-yellow"
       value={value}
+      placeholder={placeholder}
+      name={name}
     />
   );
 };
