a3646999b9afb409f1f3c8cae66dc3b109779c94
devbymarcos <marcoslopes.dev@gmail.com>
2023-12-30 17:53:02 -0300

    Adcionado logo no cadastro

--- src/app/(auth)/conta/page.tsx
+++ src/app/(auth)/conta/page.tsx
@@ -1,38 +1,83 @@
 import Link from "next/link";
 import Input from "@/components/form/Input";
 import Label from "@/components/form/Label";
+import Image from "next/image";
 
 const Login = () => {
+  async function createUser(formData: FormData) {
+    "use server";
+    const rawFormData = {
+      first_name: formData.get("first_nam"),
+      last_name: formData.get("last_name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    console.log(rawFormData);
+  }
+
   return (
     <>
-      <section className="grid grid-cols-2">
-        <div>
-          <div className="mb-3 p-8 w-full max-w-[300px] mx-auto">
-            <h1 className="mb-2 font-bold">Crie uma conta</h1>
-            <Link className="" href="/login">
-              Ja tenho uma Conta
-            </Link>
+      <section className="grid md:grid-cols-2   ">
+        <div className="self-center px-4">
+          <div className="mb-3 w-full px-2 mx-auto">
+            <h1 className="mb-2 text-lg font-bold  dark:text-base-white text-base-black">
+              Realize o cadastro para acessar
+            </h1>
           </div>
-          <p className="text-center my-6">Ou</p>
-          <div className="w-full max-w-[300px] mx-auto bg-slate-200  p-8 rounded-lg">
-            <form>
+
+          <div className="w-full  mx-auto bg-base-white dark:bg-base-black  p-8 rounded-lg">
+            <form action={createUser}>
+              <div className="mb-6">
+                <Label>Nome</Label>
+                <Input
+                  type="text"
+                  placeholder="Primeiro nome"
+                  name="first_name"
+                />
+              </div>
+              <div className="mb-6">
+                <Label>Sobre Nome</Label>
+                <Input type="text" placeholder="Último nome" name="last_name" />
+              </div>
               <div className="mb-6">
                 <Label>Email</Label>
-                <Input type="text" />
+                <Input
+                  type="text"
+                  placeholder="Seu email será seu login"
+                  name="email"
+                />
               </div>
               <div className="mb-12">
                 <Label>Password</Label>
-                <Input type="password" />
+                <Input
+                  type="password"
+                  placeholder="mínimo 8 caracteres"
+                  name="password"
+                />
               </div>
-              <div>
-                <Link href="/">
-                  <button className="bg-base-yellow px-2 py-3 font-bold block w-full rounded-sm">
-                    Login
-                  </button>
-                </Link>
+              <div className="mb-12">
+                <button className="bg-base-yellow px-2 py-3 font-bold  w-full rounded-sm">
+                  Registrar
+                </button>
               </div>
+              <Link
+                className="my-4 text-sm dark:text-base-white text-base-black"
+                href="/login"
+              >
+                Ja tem uma conta ? Acesse aqui
+              </Link>
             </form>
           </div>
+        </div>
+        <div className="w-full hidden md:flex items-center justify-center   bg-base-yellow">
+          <Image
+            src="/images/logo/logo-svg.svg"
+            width={100}
+            height={100}
+            alt="Logo do aplicativo"
+            className=" shadow-lg rounded-3xl  "
+          />
         </div>
       </section>
     </>
