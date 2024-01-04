ce8a707ac60010b213e39d041dd2ef8669a9f3ef
devbymarcos <marcoslopes.dev@gmail.com>
2024-01-04 13:30:29 -0300

    feat: novo end point

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,16 @@
+import { cookies } from "next/headers";
 const URL = process.env.API_HOST;
+const token: string | undefined = cookies().get("token")?.value;
 
 //LOGIN INIT
 interface ApiRequestOptions {
   method: string;
-  body: FormData | string;
+  body?: FormData | string;
   cache?: RequestCache | undefined;
+  headers?: {
+    "Content-Type": string;
+    Authorization: string;
+  };
 }
 
 interface ApiRequest {
@@ -16,8 +22,22 @@ export const postLoginApi = (dataForm: FormData): ApiRequest => {
   return {
     url: `${URL}/login`,
     options: {
-      method: "POST",
+      method: "POSt",
       body: dataForm,
+      cache: "no-store",
+    },
+  };
+};
+
+export const getDashApi = (wallet_id: number): ApiRequest => {
+  return {
+    url: `${URL}/dash?wallet_id=${wallet_id}`,
+    options: {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       cache: "no-store",
     },
   };
