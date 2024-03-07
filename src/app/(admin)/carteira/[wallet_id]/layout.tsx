d587866a475885c7f29a461bef8c16b357e2e57b
devbymarcos <marcoslopes.dev@gmail.com>
2024-03-06 21:02:56 -0300

    fix: nome da carteira no submenu ok feito

--- src/app/(admin)/carteira/[wallet_id]/layout.tsx
+++ src/app/(admin)/carteira/[wallet_id]/layout.tsx
@@ -4,6 +4,8 @@ import DashIcon from "@/components/icons/DashIcon";
 import ListIcon from "@/components/icons/ListIcon";
 import TransfIcon from "@/components/icons/TransfIcon";
 import CardStyle from "@/components/cards/CardStyle";
+import { getWalletApi } from "@/http/api";
+import { cookies } from "next/headers";
 
 type PropslayoutWallet = {
   params: {
@@ -12,7 +14,22 @@ type PropslayoutWallet = {
   children: React.ReactNode;
 };
 
-const layoutWallet = ({ children, params }: PropslayoutWallet) => {
+async function getWallet() {
+  const token: string | undefined = cookies().get("token")?.value;
+  const { url, options } = getWalletApi(token);
+  const response = await fetch(url, options);
+  return response.json();
+}
+
+const layoutWallet = async ({ children, params }: PropslayoutWallet) => {
+  const wallets = await getWallet();
+  let walletActivate: string = "";
+  wallets.data.forEach((wallet: any) => {
+    if (wallet.id == params.wallet_id) {
+      walletActivate = wallet.name;
+    }
+  });
+
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between md:gap-12 items-center mb-5">
@@ -46,17 +63,15 @@ const layoutWallet = ({ children, params }: PropslayoutWallet) => {
             </li>
           </ul>
         </nav>
-        {
-          //TODO NESTE CARD É PRECISO DEFINIR COMO VAMOS TRAZER O NOME DA CARTEIRA ATIVA
-        }
+
         <CardStyle>
           <div className="flex gap-4 items-center">
             <div>
               <h3 className="text-sm text-base-secondary uppercase font-bold">
-                Carteira Ativa
+                carteira Ativa
               </h3>
               <p className="text-2xl text-base-black dark:text-base-white font-bold">
-                Minha carteira
+                {walletActivate}
               </p>
             </div>
           </div>
