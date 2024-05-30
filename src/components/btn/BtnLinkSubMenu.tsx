14eaa04fa8e2d2125157a372199ba86504c64b7f
devbymarcos <marcoslopes.dev@gmail.com>
2024-05-30 12:19:11 -0300

    style: icon  on btn

--- src/components/btn/BtnLinkSubMenu.tsx
+++ src/components/btn/BtnLinkSubMenu.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 type Props = {
   children: React.ReactNode;
   href: string;
+  title: string;
 };
 
-const BtnLinkSubMenu = ({ children, href }: Props) => {
+const BtnLinkSubMenu = ({ children, href, title }: Props) => {
   return (
     <Link
       href={href}
+      title={title}
       className="bg-base-secondary px-5 py-3  text-base-white flex gap-3 rounded-full mb:rounded-sm"
     >
       {children}
