855eb8663da01660440cdf28eb3ea5bfbb88b049
devbymarcos <marcoslopes.dev@gmail.com>
2024-09-29 19:00:04 -0300

    style:  ajuste de lib para fade-in da modal

--- src/components/modal/Modalchoosetransaction.tsx
+++ src/components/modal/Modalchoosetransaction.tsx
@@ -2,6 +2,7 @@
 import {
   Description,
   Dialog,
+  DialogBackdrop,
   DialogPanel,
   DialogTitle,
 } from "@headlessui/react";
@@ -38,25 +39,25 @@ export const ModalChooseTransaction = ({
           setIsOpen(false);
         }}
         as="div"
-        className="relative z-10 focus:outline-none"
+        className="relative z-10 focus:outline-none duration-300"
       >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-black bg-opacity-25">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-black/50 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+        />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto ">
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
               className="w-full max-w-md rounded-xl bg-white px-6 py-10 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
-              {/* <BtnLinkSubMenu
-                title="Adicionar transação"
-                
-              >
-                <Plus color="#fff" size={15} />
-                <p className="hidden md:block text-sm">Adicionar</p>
-              </BtnLinkSubMenu> */}
               <DialogTitle className="text-center mb-6 text-base-secondary font-bold">
-                Escolha a Transação
+                O que você que adicionar ?
               </DialogTitle>
               <Link
+                onClick={() => {
+                  setIsOpen(false);
+                }}
                 href={`/carteira/${walletId}/transacoes/nova`}
                 className="border border-green-400 px-4 py-4 text-center   font-bols rounded-lg flex justify-between items-center font-bold text-green-800 mb-3"
               >
@@ -64,6 +65,9 @@ export const ModalChooseTransaction = ({
                 <Plus color="#166534" size={15} />
               </Link>
               <Link
+                onClick={() => {
+                  setIsOpen(false);
+                }}
                 href={`/carteira/${walletId}/transacoes/nova`}
                 className="border border-red-400 px-4 py-4 text-center   font-bols rounded-lg flex justify-between items-center font-bold text-red-800 mb-3"
               >
@@ -71,6 +75,9 @@ export const ModalChooseTransaction = ({
                 <Minus color="#166534" size={15} />
               </Link>
               <Link
+                onClick={() => {
+                  setIsOpen(false);
+                }}
                 href={`/carteira/${walletId}/transacoes/transferencia`}
                 className="border border-blue-400 px-4 py-4 text-center   font-bols rounded-lg flex justify-between items-center font-bold text-blue-800 mb-3"
               >
