0cb9b4ac4a9eb777828ad38b5a8b90dc506b0d72
devbymarcos <marcoslopes.dev@gmail.com>
2024-11-10 09:42:09 -0300

    add:nprogressBar

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -3,7 +3,7 @@ import PrivateRouter from "./PrivateRouter";
 import { montserrat } from "@/fonts";
 import Header from "@/components/header/Header";
 import MenuSideBar from "@/components/menu/MenuSidebar";
-
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 export default function RootLayout({
   children,
 }: {
@@ -18,6 +18,12 @@ export default function RootLayout({
           className={`container-custom min-h-screen mx-auto pb-40 md:pb-0   mt-6 ${montserrat.className}`}
         >
           {children}
+          <ProgressBar
+            height="4px"
+            color="#e67e22"
+            options={{ showSpinner: false }}
+            shallowRouting
+          />
         </main>
       </PrivateRouter>
     </>
